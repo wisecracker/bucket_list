c15c042e99e2871de49b4e7eedf290b6ce7881f5
OrenBNV <orenbnv@gmail.com>
2013-04-29 14:36:50 -0400

    added comment controller tests

--- test/functional/comments_controller_test.rb
+++ test/functional/comments_controller_test.rb
@@ -4,4 +4,23 @@ class CommentsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+ 	context "a Comment"
+	  setup do
+			@todo_item = TodoItem.create
+			post comments_path, :comment => { :body => "Blah", :to_item_id => @todo_item.id }
+		end
+
+		should "increase the comment count" do
+			assert_equal 1, Comment.count
+		end
+
+		should "have an associated todo item" do
+			assert_equal assigns(:comment).todo_item, @todo_item
+		end
+
+		should "gives a HTTP 200" do
+			assert_last_response :success
+		end
+	end
+	
 end
