a30b4b2a594840f4869912d0769754f70993ccdd
OrenBNV <orenbnv@gmail.com>
2013-04-29 21:47:57 -0400

    added test to todo_items controller to test creating a new todo_item

--- test/functional/todo_items_controller_test.rb
+++ test/functional/todo_items_controller_test.rb
@@ -36,6 +36,23 @@ class TodoItemsControllerTest < ActionController::TestCase
 				assert_equal TodoItem.find(1), assigns(:todo_item)
 			end
 		end
+
+
+		context "create a new todo item" do
+			setup do
+				post :create, :todo_item => { :name => "todo item name" }
+			end
+
+			should "gives a HTTP 2000" do
+				assert_response :success
+			end
+
+			should "increase the todo items count" do
+				assert_equal 4, TodoItem.count
+				assert_equal "todo item name", TodoItem.find(4).name
+			end
+		end
+
 	end
 
 end
