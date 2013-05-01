0afd17d580c6fcfaf8dc5adf58f76b90654f43d0
OrenBNV <orenbnv@gmail.com>
2013-05-01 15:35:41 -0400

    added JS to mkae all related todo_items appear on click on destination

--- app/assets/javascripts/destinations.js
+++ app/assets/javascripts/destinations.js
@@ -1,3 +1,41 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+
+	
+
+	$('.destinations').children().each( function() {
+			console.log(this);
+
+			this.onmouseover = function() {
+    		this.style.backgroundColor = 'rgb(219, 189, 189)';
+				};
+
+			this.onmouseout = function() {
+    		this.style.backgroundColor = 'white';
+				};	
+
+			this.onclick = function() {
+				this.style.backgroundColor = 'green';
+				var destinationId = this.dataset.itemId
+				showByDestination(destinationId);
+				};
+
+	});
+
+
+	function showByDestination(id) {
+		
+		$('[class=hide-me]').each(function() {		
+				var i = this.dataset.itemId
+				
+				if (i==id){			
+						this.style.display = "inherit";
+				}	
+				else{
+					this.style.display = "none";
+				}					
+		});	
+	};
+
+});
+
 
